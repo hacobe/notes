7bf000b06ebe6065bfec00a62338b25d4808c765
hacobe <91226467+hacobe@users.noreply.github.com>
2023-04-24 15:46:20 -0700

    Rename lo and hi to start and end

--- binary_search.py
+++ binary_search.py
@@ -1,48 +1,49 @@
 """Binary search.
 
 Implementation tips:
-- We could split the array in half, but better to pass in the low and high indices
+- We could split the array in half, but better to pass in the start and end indices
 - Be careful about inclusive vs. exclusive indices
 - Be careful about calculating the mid point
 - Remember that the input must be sorted
 - Remember that for the recursive implementation you have to return an index into the original array
 """
-
 import sys
 
-# [lo, hi)
-def iterative_binary_search(arr, val, lo, hi):
-	# Be careful about this inequality
-	while lo < hi:
-		mid = lo + ((hi - lo) // 2)
+# [start, end)
+def iterative_binary_search(arr, val, start, end):
+	# Be careful about tends inequality
+	# Maintain a loop invariant that val is in [start, end)
+	while start < end:
+		mid = start + ((end - start) // 2)
 		if val == arr[mid]:
 			return mid
 		elif val < arr[mid]:
-			hi = mid
+			# ..., val, ..., arr[mid], ...
+			end = mid
 		else:
-			lo = mid + 1
+			start = mid + 1
 	return -1
 
-# [lo, hi)
-def recursive_binary_search(arr, val, lo, hi):
-	# Be careful about this inequality
-	if lo >= hi:
+# [start, end)
+def recursive_binary_search(arr, val, start, end):
+	# Be careful about tends inequality
+	if start >= end:
 		return -1
-	mid = lo + ((hi - lo) // 2)
+	mid = start + ((end - start) // 2)
 	if val == arr[mid]:
 		return mid
 	elif val < arr[mid]:
-		return recursive_binary_search(arr, val, lo, mid)
+		return recursive_binary_search(arr, val, start, mid)
 	else:
-		return recursive_binary_search(arr, val, mid + 1, hi)
+		return recursive_binary_search(arr, val, mid + 1, end)
 
 def binary_search(arr, val, recursive=False):
-	lo = 0
-	hi = len(arr)
+	start = 0
+	end = len(arr)
 	if recursive:
-		return recursive_binary_search(arr, val, lo, hi)
+		return recursive_binary_search(arr, val, start, end)
 	else:
-		return iterative_binary_search(arr, val, lo, hi)
+		return iterative_binary_search(arr, val, start, end)
 
 # Test both recursive=True and recursive=False with:
 # https://practice.geeksforgeeks.org/problems/who-will-win/0
@@ -56,3 +57,19 @@ def main(stdin, recursive=False):
 			print(1)
 		else:
 			print(-1)
+
+
+if __name__ == "__main__":
+	recursive = False
+	assert binary_search([], 3, recursive) == -1
+	assert binary_search([1], 3, recursive) == -1
+	assert binary_search([4], 3, recursive) == -1
+	assert binary_search([3], 3, recursive) == 0
+	nums = [-5, -3, 0, 4] # even
+	for i, num in enumerate(nums):
+		assert binary_search(nums, num, recursive) == i
+	nums = [-5, -3, 0, 4, 20] # odds
+	for i, num in enumerate(nums):
+		assert binary_search(nums, num, recursive) == i
+	assert binary_search(nums, -7, recursive) == -1
+	assert binary_search(nums, 21, recursive) == -1
